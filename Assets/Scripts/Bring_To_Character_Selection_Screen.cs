86d974d2c73a31602e3718e2e1207f1bca87853a
ForTimeBeing <idontknowwhattoputt@gmail.com>
2019-11-24 12:17:41 -0500

    Add - Main Menu, Folders, Scenes
    Reorganized, Added main menu + scenes

---
+++ Assets/Scripts/Bring_To_Character_Selection_Screen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bring_To_Character_Selection_Screen : MonoBehaviour {
+
+	public Button yourButton;
+
+	void Start () {
+		//Button btn = yourButton.GetComponent<Button>();
+		yourButton.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick(){
+		Debug.Log ("You have clicked the button!");
+	}
+}
