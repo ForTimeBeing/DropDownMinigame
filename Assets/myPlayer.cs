8ad1b48177f895eaa698b05fd4d39d6a18e7ff09
ForTimeBeing <idontknowwhattoputt@gmail.com>
2019-11-24 11:19:11 -0500

    Init
    Init commit

---
+++ Assets/myPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myPlayer : MonoBehaviour {
+
+	public float speed;                //Floating point variable to store the player's movement speed.
+
+	private Rigidbody2D rb2d;        //Store a reference to the Rigidbody2D component required to use 2D Physics.
+	// Use this for initialization
+	private float moveHorizontal;
+
+	void Start () {
+			rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown == false){
+			Vector2    v = rb2d.velocity;
+			v.x = 0f;
+			rb2d.velocity = v;
+			//rb2d.velocity = new Vector2(0,Input.GetAxis("Verticle"));
+			//rb2d.angularVelocity = 0f;
+		}
+		moveHorizontal = Input.GetAxis ("Horizontal");
+		//Store the current horizontal input in the float moveHorizontal.
+        
+ 		 //Store the current vertical input in the float moveVertical.
+        
+		  //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2 (moveHorizontal,0);
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        rb2d.AddForce (movement * speed);
+	}
+}
