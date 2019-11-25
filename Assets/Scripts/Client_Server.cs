f687a86a37a839abf72baaeca7a668dbb61ba952
ForTimeBeing <idontknowwhattoputt@gmail.com>
2019-11-25 00:47:54 -0500

    Client can connect to server
    Client can connect to server - Can now send messages back and forth

--- Assets/Scripts/Client_Server.cs
+++ Assets/Scripts/Client_Server.cs
@@ -8,6 +8,7 @@ public class Client_Server : MonoBehaviour {
 	private const int PORT = 26000;
 	private const int WEB_PORT = 26001;
 	private const string SERVER_IP = "127.0.0.1";
+	private const int BYTE_SIZE = 1024;
 
 	private byte reliableChannel;
 	private int hostID;
@@ -18,6 +19,9 @@ public class Client_Server : MonoBehaviour {
 	private void Start(){
 		DontDestroyOnLoad(gameObject);
 		Init();
+	}
+	private void Update(){
+		UpdateMessagePump();
 	}
 	public void Init(){
 		NetworkTransport.Init();
@@ -39,6 +43,42 @@ public class Client_Server : MonoBehaviour {
 #endif
 		Debug.Log(string.Format("Attempting to connect on {0}...", SERVER_IP));
 		isStarted = true;
+	}
+	public void UpdateMessagePump(){
+		if(!isStarted)
+			return;
+		
+		int recHostID;		//Is this from Web? Or standalone
+		int connectionID;	//Which user is sending my this?
+		int channelID;		//Which lane is he senting that message from
+
+		byte[] recBuffer = new byte[BYTE_SIZE];
+		int dataSize;
+
+		NetworkEventType type = NetworkTransport.Receive(out recHostID, out connectionID, out channelID, recBuffer, BYTE_SIZE, out dataSize, out error);
+		switch(type){
+
+			case NetworkEventType.Nothing:
+				break;
+
+			case NetworkEventType.ConnectEvent:
+				Debug.Log("We have connected to the server");
+				break;
+
+			case NetworkEventType.DisconnectEvent:
+				Debug.Log("We have been disconnected");
+				break;
+
+			case NetworkEventType.DataEvent:
+				Debug.Log("Data");
+				break;
+
+			default:
+			case NetworkEventType.BroadcastEvent:
+				Debug.Log("Unexpected network event type");
+				break;
+				
+		}
 	}
 	public void Shutdown(){
 		isStarted = false;
