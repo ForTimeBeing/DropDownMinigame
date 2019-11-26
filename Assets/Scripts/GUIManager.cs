b47122772feda2c8f9c56bef2298887dc4ab9248
ForTimeBeing <idontknowwhattoputt@gmail.com>
2019-11-26 01:08:42 -0500

    Lobby List creates new lobbies dynamically
    Lobby List creates new lobbies dynamically
    Need to add player count still and possibily ping

--- Assets/Scripts/GUIManager.cs
+++ Assets/Scripts/GUIManager.cs
@@ -8,39 +8,36 @@ public class GUIManager : MonoBehaviour {
 
 	public InputField lobbyTextBoxField;
 
-	public GameObject lobbyGameObject;
+	public GameObject contentPrefab;
 
-	public GameObject scrollViewParent;
-
-	public ScrollRect myScrollBar;
+	public GameObject contentParent;
 
 
 	int amountOfLobbies = 0;
 	//On load check to see how many lobby's there are and populate them. Store the amount
 	void Start () {
 		//Button btn = yourButton.GetComponent<Button>();
-		createButton.onClick.AddListener(TaskOnClick);
+		createButton.onClick.AddListener(CreateLobby);
 		lobbyTextBoxField.characterLimit = 20;
 		lobbyTextBoxField.lineType = InputField.LineType.SingleLine;
 		lobbyTextBoxField.contentType = InputField.ContentType.Alphanumeric;
 	}
+
 	
-	void TaskOnClick(){
+
+	void CreateLobby(){
 		if (string.IsNullOrEmpty(lobbyTextBoxField.text)){
 			//Display pop up saying textbox cannot be empty
 			Debug.Log("Inputbox is empty");
 		}
 		else{
 			// Use server amount to append new lobbies
-			// Instantiate at position (0, 0) and zero rotation.
-			//lobbyGameObject = new GameObject();
-			//lobbyGameObject.AddComponent<Text>();
-			Text lobbyText = lobbyGameObject.GetComponent<Text>(); 
+			Text lobbyText = contentPrefab.GetComponent<Text>(); 
 			lobbyText.text = lobbyTextBoxField.text;
-        	GameObject newLobbyGameObject = Instantiate(lobbyGameObject, new Vector2(0, 0), Quaternion.identity);
-			newLobbyGameObject.transform.SetParent (scrollViewParent.gameObject.transform);
-			newLobbyGameObject.transform.localPosition = new Vector2(0,amountOfLobbies * 20);
-			amountOfLobbies = amountOfLobbies + 1;
+        	GameObject newLobbyGameObject = Instantiate(contentPrefab, new Vector2(0, 0), Quaternion.identity, contentParent.gameObject.transform);
+			newLobbyGameObject.gameObject.transform.localPosition = new Vector2(0,amountOfLobbies * 20);
+
+			amountOfLobbies = amountOfLobbies - 1;
 			Debug.Log("we good fam");
 		}
 		//SceneManager.LoadScene("Character_Selection");
