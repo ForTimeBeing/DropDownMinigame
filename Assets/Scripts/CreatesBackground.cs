782c5e3f3349a5712cb59933c9e57ee7b6967869
ForTimeBeing <idontknowwhattoputt@gmail.com>
2019-11-26 22:39:05 -0500

    Added clickbox for lobby selection
    Added clickbox for lobby selection. Need to add background color after being clicked and removed when a new one is clicked

---
+++ Assets/Scripts/CreatesBackground.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CreatesBackground : MonoBehaviour {
+
+	public GameObject backgroundPrefab;
+
+	void OnMouseDown()
+    {
+		Debug.Log("SDSD");
+      GameObject newLobbyGameObject = Instantiate(backgroundPrefab, new Vector2(0, 0), Quaternion.identity, gameObject.transform);
+    }
+}
--- Assets/Scripts/GUIManager.cs
+++ Assets/Scripts/GUIManager.cs
@@ -12,7 +12,6 @@ public class GUIManager : MonoBehaviour {
 
 	public GameObject contentParent;
 
-
 	int amountOfLobbies = 0;
 	//On load check to see how many lobby's there are and populate them. Store the amount
 	void Start () {
@@ -23,19 +22,23 @@ public class GUIManager : MonoBehaviour {
 		lobbyTextBoxField.contentType = InputField.ContentType.Alphanumeric;
 	}
 
-	
-
 	void CreateLobby(){
 		if (string.IsNullOrEmpty(lobbyTextBoxField.text)){
 			//Display pop up saying textbox cannot be empty
 			Debug.Log("Inputbox is empty");
 		}
 		else{
-			// Use server amount to append new lobbies
+			// Use server amount to append new lobbies store new lobbies on server
 			Text lobbyText = contentPrefab.GetComponent<Text>(); 
 			lobbyText.text = lobbyTextBoxField.text;
         	GameObject newLobbyGameObject = Instantiate(contentPrefab, new Vector2(0, 0), Quaternion.identity, contentParent.gameObject.transform);
 			newLobbyGameObject.gameObject.transform.localPosition = new Vector2(0,amountOfLobbies * 20);
+			newLobbyGameObject.AddComponent<CreatesBackground>();
+			BoxCollider2D myBoxCollider = newLobbyGameObject.AddComponent<BoxCollider2D>();
+			myBoxCollider.offset = new Vector2(-1.52f,-1.05f);
+			myBoxCollider.size = new Vector2(503,21);
+
+
 
 			amountOfLobbies = amountOfLobbies - 1;
 			Debug.Log("we good fam");
