cd3dfc3315b5d734f9200068992242c89dad72cb
ForTimeBeing <idontknowwhattoputt@gmail.com>
2019-12-01 20:32:35 -0500

    Made mainmap scaleable + added compton
    Main game is now scaleable and reuseable. Make sure the sizes of images are correct

---
+++ Assets/Scripts/ItemCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollision : MonoBehaviour
+{
+    public ParticleSystem emit;
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            CmdDetachParticles();
+            Destroy(gameObject);
+        }
+    }
+
+    // Call this immediately before you destroy your missile
+    void CmdDetachParticles()
+    {
+        // This splits the particle off so it doesn't get deleted with the parent
+        emit.transform.parent = null;
+
+        // this stops the particle from creating more bits
+        emit.Stop();
+    }
+
+
+}
---
+++ Assets/Scripts/SpawnItems.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SpawnItems : MonoBehaviour
+{
+    public GameObject prefab;
+    float timeLeft = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float randomSpawnTime = (Random.Range(.2f, .6f));
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            Invoke("Spawn", 0f);
+            timeLeft = randomSpawnTime;
+        }
+    }
+    void Spawn()
+    {
+        float randomNumber = Random.Range(-4.5f, 5f);
+        GameObject dip = Instantiate(prefab);
+        Destroy(dip, 6);
+        dip.transform.position = new Vector2(randomNumber, 6);
+    }
+
+
+}
