6bc1f3caedede27ce1e6d56d7a4e78bfc307b265
ForTimeBeing <idontknowwhattoputt@gmail.com>
2019-12-01 21:35:53 -0500

    Added Compton Special
    Added Compton Special

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+
+    float shakeAmount = 0;
+
+    public void Shake(float amt, float length)
+    {
+        shakeAmount = amt;
+        InvokeRepeating("BeginShake", 0, 1f);
+        Invoke("StopShake", length);
+
+    }
+    void BeginShake()
+    {
+        if (shakeAmount > 0)
+        {
+            Vector3 camPos = gameObject.transform.position;
+
+            float offsetX = Random.value * shakeAmount * 2 - shakeAmount;
+            float offsetY = Random.value * shakeAmount * 2 - shakeAmount;
+            camPos.x += offsetX;
+            camPos.y += offsetY;
+
+            gameObject.transform.position = camPos;
+        }
+    }
+    void Update()
+    {
+        //Shake(0.3f,.3f);
+    }
+    void StopShake()
+    {
+        CancelInvoke("BeginShake");
+        gameObject.transform.localPosition = Vector3.zero;
+    }
+}
